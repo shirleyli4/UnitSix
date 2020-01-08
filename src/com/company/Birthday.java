dfe8309df6212243641d9bdceea05d75423fb922
shuzhou li <shuzhou.li@ssfs.org>
2020-01-08 10:12:57 -0500

    finished

---
+++ src/com/company/Birthday.java
@@ -0,0 +1,37 @@
+//Shirley Shuzhou Li, Birthday paradox program
+package com.company;
+
+public class Birthday {
+    public static void main (String[] args){
+        double same=0.0;
+        for(int i=0;i<1000;i++) {
+            if(hasDupes(dates(23))==true){
+                same++;
+            }
+        }
+        double percentage = (same/1000.0)*100;
+        System.out.println("The percent of times there were duplicate birthdays is "+percentage+"%");
+    }
+
+    public static int[] dates(int num){
+        int[] dates = new int[num];
+        for(int i=0;i<dates.length;i++){
+            int number = (int)(Math.random()*365);
+            dates[i]=number;
+        }
+        return dates;
+    }
+
+    public static boolean hasDupes(int[] birthdays){
+        boolean ret=false;
+        for(int i=0;i<birthdays.length;i++){
+            for(int j=i+1;j<birthdays.length;j++){
+                if(birthdays[i]==birthdays[j]){
+                    ret=true;
+                }
+            }
+        }
+        return ret;
+    }
+
+}
